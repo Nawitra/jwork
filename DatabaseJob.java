bceecf8abdfbed470cfcc0bfd5dff8b0b40de768
Nawitra <vernandowijayaputra@gmail.com>
2021-03-18 18:27:13 +0700

    Modul 2 - Revisi 3

--- DatabaseJob.java
+++ DatabaseJob.java
@@ -13,18 +13,38 @@ public class DatabaseJob
         listJob = new String[255];
     }
     
+    /**
+     * addJob untuk menambahkan pekerjaan ke dalam listJob
+     *
+     * @return false, sebagai nilai default
+     */
     public boolean addJob(Job job) {
         return false;
     }
-    
+
+    /**
+     * remove untuk menghapus entry pekerjaan dari listJob
+     *
+     * @return false, sebagai nilai default
+     */
     public boolean remove(Job job) {
         return false;   
     }
     
+    /**
+     * getJob untuk mendapatkan nilai dari objek Job
+     *
+     * @return null, karena fungsi belum selesai
+     */
     public Job getJob() {
         return null;   
     }
     
+    /**
+     * getListJob untuk mendapatkan nilai dari property listJob
+     *
+     * @return listJob, list dari pekerjaan
+     */
     public String[] getListJob() {
         return listJob;
     }
--- Recruiter.java
+++ Recruiter.java
@@ -18,47 +18,99 @@ public class Recruiter
         this.location = location;
     }
     
-    
+    /**
+     * getId untuk mendapatkan nilai dari property id
+     *
+     * @return nilai dari property id
+     */
     public int getId() {
         return this.id;
     }
     
+    /**
+     * getName untuk mendapatkan nilai dari property name
+     *
+     * @return nilai dari property name
+     */
     public String getName() {
         return this.name;
     }
     
+    /**
+     * getEmail untuk mendapatkan nilai dari property email
+     *
+     * @return nilai dari property email
+     */
     public String getEmail() {
         return this.email;
     }
     
+    /**
+     * getPhoneNumber untuk mendapatkan nilai dari property phoneNumber
+     *
+     * @return nilai dari property phoneNumber
+     */
     public String getPhoneNumber() {
         return this.phoneNumber;
     }
     
+    /**
+     * getLocation untuk mendapatkan nilai dari objek location
+     *
+     * @return nilai dari objek location
+     */
     public Location getLocation() {
         return this.location;   
     }
     
+    /**
+     * setId untuk menggantikan nilai dari property id
+     * 
+     * @param id, property yang nilainya akan digantikan, dengan tipe int
+     */
     public void setId(int id) {
         this.id = id;
     }
     
+    /**
+     * setEmail untuk menggantikan nilai dari property email
+     * 
+     * @param email, property yang nilainya akan digantikan, dengan tipe String
+     */
     public void setEmail(String email) {
         this.email = email;
     }
     
+    /**
+     * setName untuk menggantikan nilai dari property name
+     * 
+     * @param name, property yang nilainya akan digantikan, dengan tipe String
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * setPhoneNumber untuk menggantikan nilai dari property phoneNumber
+     * 
+     * @param phoneNumber, property yang nilainya akan digantikan, dengan tipe String
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
     
+    /**
+     * setLocation untuk menggantikan nilai dari property pada objek location
+     * 
+     * @param location, property yang nilainya akan digantikan, dengan tipe Location
+     */
     public void setLocation(Location location) {
         this.location = location;    
     }
     
+    /**
+     * printData untuk menampilkan nilai dari property name
+     */
     public void printData() {
         System.out.println(getName());
     }
