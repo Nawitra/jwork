4cdeb937e82973b0d656c1b491ef957abff6bc95
Nawitra <vernandowijayaputra@gmail.com>
2021-04-03 09:19:38 +0700

    Modul 4 - Post Test

---
+++ BankPayment.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class BankPayment here.
+ *
+ * @author Vernando Wijaya Putra
+ * @version 2021.3.4
+ */
+public class BankPayment extends Invoice
+{
+    private static final PaymentType PAYMENT_TYPE = PaymentType.BankPayment;
+    private int adminFee;
+    
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus) {
+        super(id, job, date, jobseeker, invoiceStatus);    
+    }
+    
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus, int adminFee) {
+        super(id, job, date, jobseeker, invoiceStatus);
+        this.adminFee = adminFee;
+    }
+    
+    @Override
+    public PaymentType getPaymentType() {
+        return PAYMENT_TYPE;    
+    }
+    
+    public int getAdminFee() {
+        return adminFee;    
+    }
+    
+    public void setAdminFee(int adminFee) {
+        this.adminFee = adminFee;    
+    }
+    
+    @Override
+    public void setTotalFee() {
+        if (adminFee != 0) {
+            super.totalFee = getJob().getFee() - adminFee;
+        } else {
+            super.totalFee = getJob().getFee();
+        }
+    }
+    
+    @Override
+    public void printData() {
+        System.out.println("===================== Invoice =====================");
+        System.out.println("ID: " + getId());
+        System.out.println("Job: " + getJob().getName());
+        System.out.println("Date: " + getDate());
+        System.out.println("Jobseeker: " + getJobseeker().getName());
+        System.out.println("Admin Fee: " + adminFee);
+        System.out.println("Total Fee: " + super.totalFee);
+        System.out.println("Status: " + getInvoiceStatus());
+        System.out.println("Payment Type: " + PAYMENT_TYPE);
+    }
+}
--- JWork.java
+++ JWork.java
@@ -8,6 +8,9 @@
 public class JWork
 {
     public static void main(String[] args) {
+        /*
+         * CS modul 4
+         *
         Jobseeker jobseekerCS4 = new Jobseeker(1, "Job Seek", "jobseeker@test.com", "personthere", "28 february");
         Bonus bonusCS41 = new Bonus(1, "AX443GG", 1000000, 5000, true);
         Bonus bonusCS42 = new Bonus(1, "BX443JJ", 5000, 100000, true);
@@ -26,5 +29,20 @@ public class JWork
         wallet2.printData();
         wallet3.printData();
         wallet4.printData();
+        */
+       
+        /*
+         * Post Test Modul 4
+         */
+        Jobseeker jobseekerPT4 = new Jobseeker(1, "Vocation Seeker", "jobseeker@test.com", "thyshallbe", "29 february");
+        Location locationPT4 = new Location("Sumatera Selatan", "Lubuklinggau", "Tempat lahir");
+        Recruiter recruiterPT4 = new Recruiter(2, "Vernando Wijaya Putra", "vernandowijayaputra@gmail.com", "087743031638", locationPT4);
+        Job jobPT4 = new Job(1, 18000, "programmer", recruiterPT4, JobCategory.UI);
+        BankPayment bankPT41 = new BankPayment(1, jobPT4, "3 April 2021", jobseekerPT4, InvoiceStatus.Finished);
+        BankPayment bankPT42 = new BankPayment(1, jobPT4, "3 April 2021", jobseekerPT4, InvoiceStatus.Finished, 5000);
+        bankPT41.setTotalFee();
+        bankPT42.setTotalFee();
+        bankPT41.printData();
+        bankPT42.printData();
     }
 }
